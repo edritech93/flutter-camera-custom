87a0f5177e9a0e297083980501b9f3c08bdcec6d
aditya <aditya@leher.ai>
2019-06-13 20:27:58 +0530

    bitrate-adjust

--- android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
+++ android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
@@ -450,7 +450,9 @@ public class CameraPlugin implements MethodCallHandler {
       mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
       if (enableAudio) mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
       mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-      mediaRecorder.setVideoEncodingBitRate(1024 * 1000);
+      //mediaRecorder.setVideoEncodingBitRate(1024 * 1000);
+      mediaRecorder.setVideoEncodingBitRate(3048 * 1000);
+
       if (enableAudio) mediaRecorder.setAudioSamplingRate(16000);
       mediaRecorder.setVideoFrameRate(27);
       mediaRecorder.setVideoSize(videoSize.getWidth(), videoSize.getHeight());
