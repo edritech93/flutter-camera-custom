319626378eb84fbe2694570510cc43bbd0e3291d
aditya <aditya@leher.ai>
2019-06-14 17:48:01 +0530

    quality

--- android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
+++ android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
@@ -451,7 +451,8 @@ public class CameraPlugin implements MethodCallHandler {
       if (enableAudio) mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
       mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
       //mediaRecorder.setVideoEncodingBitRate(1024 * 1000);
-      mediaRecorder.setVideoEncodingBitRate(10000000);
+      mediaRecorder.setVideoEncodingBitRate(40000000);
+
       
 
       if (enableAudio) mediaRecorder.setAudioSamplingRate(16000);
